e5e0c66b9c08f2e4af6cba05761c2c17b8adaf10
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-03-25 22:19:59 +0100

    Profile component
    User information

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from './Profile';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101'
+    // }}
     >
-      React homework template
+      {/* React homework template */}
+      <Profile></Profile>
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import user from '../user.json';
+
+export const Profile = () => {
+  const { username, tag, location, avatar, stats } = user;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
