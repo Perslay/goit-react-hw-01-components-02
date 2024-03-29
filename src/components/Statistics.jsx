bacd52d4db6f31a5c803c0e8291338828f36966a
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-03-29 19:12:12 +0100

    Statistics color correction
    Fixed a problem, where background color would not appear sometimes. The text color would blend with background then.

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -3,7 +3,10 @@ import css from '../css/Statistics.module.css';
 
 // randomise background color for list item
 const randomiseColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // return randomColor.trim() === '#' ? '#FF0000' : randomColor;
+  const paddedColor = randomColor.padStart(6, '0');
+  return `#${paddedColor}`;
 };
 
 // calculate background lightness
@@ -12,7 +15,7 @@ const calculateLuminance = color => {
     hex
       .replace(
         /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
-        (m, r, g, b) => '#' + r + r + g + g + b + b
+        (r, g, b) => '#' + r + r + g + g + b + b
       )
       .substring(1)
       .match(/.{2}/g)
