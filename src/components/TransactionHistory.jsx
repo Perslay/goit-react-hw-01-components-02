65a8a3ebbfdfb44364ae0da92ee56957674dd80d
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-03-30 14:05:40 +0100

    Transaction History styles
    For all screen sizes.
    In FriendListItems fixed text overflow.

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -4,25 +4,26 @@ import css from '../css/TransactionHistory.module.css';
 export const TransactionHistory = ({ items }) => {
   const transactionsList = items.transactionsArr;
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {transactionsList.map(transaction => (
-          <tr key={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
+    <div className={css.transactionHistory}>
+      <table className={css.table}>
+        <thead className={css.tableHead}>
+          <tr className={css.tableRow}>
+            <th className={css.tableHeading}>Type</th>
+            <th className={css.tableHeading}>Amount</th>
+            <th className={css.tableHeading}>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {transactionsList.map(transaction => (
+            <tr className={css.tableRow} key={transaction.id}>
+              <td className={css.tableData}>{transaction.type}</td>
+              <td className={css.tableData}>{transaction.amount}</td>
+              <td className={css.tableData}>{transaction.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
